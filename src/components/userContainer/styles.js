1dec9b5d72179d17e647970f97d13d56dd322bf1
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-05-31 09:24:40 -0300

    change height and border-bottom color

--- src/components/userContainer/styles.js
+++ src/components/userContainer/styles.js
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 
 export const Section = styled.section`
   width: 400px;
-  height: 300px;
+  height: 350px;
   background: rgba(255, 255, 255, 0.95);
   border-radius: 10px;
-  border-bottom: 10px solid #ff7a00;
+  border-bottom: 10px solid #8a2be2;
   margin-top: 15px;
 `;
