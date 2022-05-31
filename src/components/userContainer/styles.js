7d2d6e2420cb392949f9da651046f4d5d8c2df6a
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-05-31 09:53:22 -0300

    remove height property

--- src/components/userContainer/styles.js
+++ src/components/userContainer/styles.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const Section = styled.section`
   width: 400px;
-  height: 350px;
   background: rgba(255, 255, 255, 0.95);
   border-radius: 10px;
   border-bottom: 10px solid #8a2be2;
