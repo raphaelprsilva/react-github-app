d9e664a28b8490934d7c5ad9962139a6c4c11ef5
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-05-31 08:47:44 -0300

    implements userContainer component

---
+++ src/components/userContainer/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Section = styled.section`
+  width: 400px;
+  height: 300px;
+  background: rgba(255, 255, 255, 0.95);
+  border-radius: 10px;
+  border-bottom: 10px solid #ff7a00;
+  margin-top: 15px;
+`;
