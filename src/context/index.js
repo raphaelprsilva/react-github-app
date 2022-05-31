00fd3d0c6725d09311b9fea91f2dce9bd9e464b2
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-05-31 18:26:31 -0300

    add context

---
+++ src/context/index.js
@@ -0,0 +1,18 @@
+import { createContext, useState } from 'react';
+
+export const context = createContext();
+
+export const ContextProvider = (props) => {
+  const [userData, setUserData] = useState({});
+
+  return (
+    <context.Provider
+      value={{
+        userData,
+        setUserData,
+      }}
+    >
+      {props.children}
+    </context.Provider>
+  );
+};
