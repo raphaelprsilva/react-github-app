5e0f3aab033e2e4350477982ba6e6c46e6a4c941
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-05-31 18:27:03 -0300

    get user data from contextAPI

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,16 +1,43 @@
+import { useContext, useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
-import { HeaderInput, HeaderInputContainer, HeaderSearchButton, HeaderSection, HeaderTitle } from './styles';
+import {
+  HeaderInput,
+  HeaderInputContainer,
+  HeaderSearchButton,
+  HeaderSection,
+  HeaderTitle,
+} from './styles';
 
-const Header = () => (
-  <HeaderSection>
-    <HeaderTitle>Perfil do Github</HeaderTitle>
-    <HeaderInputContainer>
-      <HeaderInput />
-      <HeaderSearchButton>
-        <FiSearch size={15} />
-      </HeaderSearchButton>
-    </HeaderInputContainer>
-  </HeaderSection>
-);
+import { context } from '../../context';
+import client from '../../services/client';
+
+const Header = () => {
+  const ctx = useContext(context);
+  const [searchedValue, setSearchedValue] = useState('');
+
+  async function getUserData() {
+    try {
+      const response = await client.get(`${searchedValue}`);
+      ctx.setUserData(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <HeaderSection>
+      <HeaderTitle>Perfil do Github</HeaderTitle>
+      <HeaderInputContainer>
+        <HeaderInput
+          value={searchedValue}
+          onChange={(event) => setSearchedValue(event.target.value)}
+        />
+        <HeaderSearchButton onClick={getUserData}>
+          <FiSearch size={15} />
+        </HeaderSearchButton>
+      </HeaderInputContainer>
+    </HeaderSection>
+  );
+};
 
 export default Header;
