fef9ea885429e0ca879b9d9e286494b8811056d7
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-06-01 09:25:44 -0300

    add following and followers route

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,20 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Followers from './pages/followers';
+import Following from './pages/following';
 import Home from './pages/home';
+import Repos from './pages/repos';
 
-const Routes = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" exact component={Home} />
-    </Switch>
-  </BrowserRouter>
-);
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/repos" component={Repos} />
+        <Route path="/followers" component={Followers} />
+        <Route path="/following" component={Following} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default Routes;
